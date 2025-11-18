2b21dc09c05d460f7b4068186454573a8768e369
Zhang, Xiaolin <xiaolin.zhang@intel.com>
2025-11-18 13:53:08 +0800

    benchmark: fix TypeError: unsupported operand type(s) for %: 'int' and 'str'
    failed to run benchmark.py script with TypeError on NV3090 GPU.
    
    for the test result of the following command:
    python tools/analysis_tools/benchmark.py projects/configs/flashocc/flashocc-r50.py ckpts/flashocc-r50-256x704.pth --samples 500 --log-interval 50
    
    Done image [50 / 500], fps: 24.5 img / s
    Done image [100/ 500], fps: 24.5 img / s
    Done image [150/ 500], fps: 24.5 img / s
    Done image [200/ 500], fps: 24.4 img / s
    Done image [250/ 500], fps: 24.4 img / s
    Done image [300/ 500], fps: 24.4 img / s
    Done image [350/ 500], fps: 24.3 img / s
    Done image [400/ 500], fps: 24.3 img / s
    Done image [450/ 500], fps: 24.2 img / s
    Done image [500/ 500], fps: 24.2 img / s
    Overall
    fps: 24.19 img / s
    inference time: 41.33 ms
    
    Signed-off-by: Zhang, Xiaolin <xiaolin.zhang@intel.com>

--- tools/analysis_tools/benchmark.py
+++ tools/analysis_tools/benchmark.py
@@ -20,9 +20,9 @@ def parse_args():
     parser = argparse.ArgumentParser(description='MMDet benchmark a model')
     parser.add_argument('config', help='test config file path')
     parser.add_argument('checkpoint', help='checkpoint file')
-    parser.add_argument('--samples', default=500, help='samples to benchmark')
+    parser.add_argument('--samples', type=int, default=500, help='samples to benchmark')
     parser.add_argument(
-        '--log-interval', default=50, help='interval of logging')
+        '--log-interval', type=int, default=50, help='interval of logging')
     parser.add_argument(
         '--fuse-conv-bn',
         action='store_true',
